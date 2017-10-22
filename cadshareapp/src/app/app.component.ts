350bf488d16f1ba58c99777cf66acbe08b97a578
a2un <encodedgeek@gmail.com>
2017-10-22 23:12:09 +0530

    angular material and a button

--- cadshareapp/src/app/app.component.ts
+++ cadshareapp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as mat  from '@angular/material';
+import * as anime  from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- cadshareapp/src/app/app.module.ts
+++ cadshareapp/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule,MatFormFieldModule,MatSelectModule,MatButtonModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,MatFormFieldModule,MatSelectModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
